8b994075ff4ca66370870d16dba0f492b3f00957
Tecon 1 <tecon1@Tecons-MacBook-Air.local>
2025-05-22 11:22:08 -0500

    delete services out use

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase with your service account credentials
-cred = credentials.Certificate(r"C:\Users\PC\Documents\TECON\Camaras\web\keys\Auth_firestore.json")
+cred = credentials.Certificate(r"/Users/tecon1/Documents/WebAdminCamara/keys/Auth_firestore.json")
 firebase_admin.initialize_app(cred)
 
 # Get Firestore client
